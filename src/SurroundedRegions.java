7572ebf98881a917485fb392e36aa50d932986de
nyannko <9638293+nyannko@users.noreply.github.com>
2018-12-16 23:51:00 +0100

    16/12/2018,23:51

---
+++ src/SurroundedRegions.java
@@ -0,0 +1,50 @@
+public class SurroundedRegions {
+    public void solve(char[][] board) {
+        if (board.length == 0) {
+            return;
+        }
+        int row = board.length;
+        int col = board[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 || j == 0 || i == row - 1 || j == col - 1) {
+                    if (board[i][j] == 'O') {
+                        dfs(board, i, j);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == '*') {
+                    board[i][j] = 'O';
+                } else {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = '*';
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+
+    public static void main(String[] args) {
+        char[][] board1 = {{'X', 'X', 'X', 'X'}, {'X', 'O', 'O', 'X'}, {'X', 'X', 'O', 'X'}, {'X', 'O', 'X', 'X'}};
+        SurroundedRegions s = new SurroundedRegions();
+        s.solve(board1);
+        for (char[] aBoard1 : board1) {
+            for (int j = 0; j < board1[0].length; j++) {
+                System.out.println(aBoard1[j]);
+            }
+        }
+    }
+}
