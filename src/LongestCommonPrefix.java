cc85595d9d840dc37bac8463b2142fe8729b3840
nyannko <9638293+nyannko@users.noreply.github.com>
2018-12-28 17:30:59 +0100

    28/12/2018,17:30

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,21 @@
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String pre = strs[0];
+        int i = 1;
+        while (i < strs.length) {
+            int temp = strs[i].indexOf(pre);
+            while (strs[i].indexOf(pre) != 0) {
+                pre = pre.substring(0, pre.length() - 1);
+            }
+            i++;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        LongestCommonPrefix l = new LongestCommonPrefix();
+        l.longestCommonPrefix(strs);
+    }
+}
