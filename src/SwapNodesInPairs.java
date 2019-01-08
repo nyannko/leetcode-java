ae241ecf1d2713a613de248e96ffcb2c7e8f9abd
nyannko <9638293+nyannko@users.noreply.github.com>
2019-01-08 23:59:03 +0100

    08/01/2019,23:59

---
+++ src/SwapNodesInPairs.java
@@ -0,0 +1,26 @@
+public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode point = dummy;
+        while (point.next != null && point.next.next != null) {
+            ListNode swap1 = point.next;
+            ListNode swap2 = point.next.next;
+            point.next = swap2;
+            swap1.next = swap2.next;
+            swap2.next = swap1;
+            point = swap1;
+        }
+        return dummy.next;
+    }
+
+    public ListNode swapPairs1(ListNode head) {
+        if ((head == null) || (head.next) == null) {
+            return head;
+        }
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+}
--- src/TrappingRainWater.java
+++ src/TrappingRainWater.java
@@ -1,7 +1,7 @@
 public class TrappingRainWater {
     public int trap(int[] height) {
         int i = 0, j = height.length - 1;
-        int lMax = 0, rMax=0;
+        int lMax = 0, rMax = 0;
         int maxValue = 0;
         while (i <= j) {
             lMax = Math.max(lMax, height[i]);
@@ -9,7 +9,7 @@ public class TrappingRainWater {
             if (lMax < rMax) { // decided by the lower side
                 maxValue += lMax - height[i];
                 i++;
-            } else{
+            } else {
                 maxValue += rMax - height[j];
                 j--;
             }
@@ -18,7 +18,7 @@ public class TrappingRainWater {
     }
 
     public static void main(String[] args) {
-       TrappingRainWater r = new TrappingRainWater();
-        System.out.println(r.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+        TrappingRainWater r = new TrappingRainWater();
+        System.out.println(r.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
     }
 }
