332e43d84a70c2ab881782b801c842fa198dfdea
nyannko <9638293+nyannko@users.noreply.github.com>
2019-01-10 19:00:23 +0100

    10/01/2019,19:00

---
+++ src/LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,11 @@
+public class LowestCommonAncestorofaBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+        if (l != null && r != null) {
+            return root;
+        }
+        return (l == null) ? r : l;
+    }
+}
